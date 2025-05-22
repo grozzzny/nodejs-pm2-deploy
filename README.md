f16865e1fb7a7e25b1b507c23194b3ec67e9e7eb
grozzzny <grozzzny@gmail.com>
2025-05-22 18:06:16 +0200

    add ecosystem.config.js

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 Стартеркит проекта по автоматизации деплоя фронтенда и бэкенда при помощи pm2 (pm2 deploy)
 
 # Домены приложений
-- https://pm2.grozzzny.host (79.132.138.8) - фронтенд
-- https://api.pm2.grozzzny.host (79.132.138.8) - бекенд
+- https://api.pm2.grozzzny.host (79.132.138.8:3000) - бекенд
+- https://pm2.grozzzny.host (79.132.138.8:3001) - фронтенд
+
+# Crash test:
+- http://localhost:3000/crash-test
+- http://79.132.138.8:3000/crash-test
+- https://api.pm2.grozzzny.host/crash-test
---
+++ ecosystem.config.js
@@ -0,0 +1,36 @@
+require('dotenv').config({ path: '.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/main', REPO_URL, DEPLOY_PORT
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: "backend",
+      script: "dist/app.js",
+      cwd: "./backend",
+      autorestart: true
+    },
+    {
+      name: "frontend",
+      script: "serve",
+      args: ["-s", "build", "-l", "3001"],
+      cwd: "./frontend",
+      autorestart: true
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      port: DEPLOY_PORT || 22,
+      ref: DEPLOY_REF,
+      repo: REPO_URL,
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp -P 19 ${DEPLOY_PATH}/backend/.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/backend`,
+      'post-deploy': `cd ${DEPLOY_PATH}/current && DEPLOY_PATH=${DEPLOY_PATH} ./deploy.sh`
+    },
+  },
+};
