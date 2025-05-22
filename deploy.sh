7514956df5aae884e616fc146827706f0f9c3fca
grozzzny <grozzzny@gmail.com>
2025-05-22 16:56:30 +0200

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo "Backend"
+cd "${DEPLOY_PATH}/current/backend"
+yarn install --production
+yarn build
+
+echo "Frontend"
+cd "${DEPLOY_PATH}/current/frontend"
+yarn install --production
+yarn build
+
+echo "Pm2"
+pm2 startOrRestart "${DEPLOY_PATH}/current/ecosystem.config.js" --env production
+pm2 save
+
+echo "Completed!"
