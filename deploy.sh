d70cc682cf4cfe577fa88f12c0a1f61fa149b200
grozzzny <grozzzny@gmail.com>
2025-05-22 23:32:26 +0200

    fix ecosystem.config.js

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,8 @@ cd "${DEPLOY_PATH}/current/frontend"
 yarn install --production
 yarn build
 
-echo "Pm2"
-npx pm2 startOrRestart "${DEPLOY_PATH}/current/ecosystem.config.js" --env production
-npx pm2 save
+#echo "Pm2"
+#pm2 startOrRestart "${DEPLOY_PATH}/current/ecosystem.config.js" --env production
+#pm2 save
 
 echo "Completed"
